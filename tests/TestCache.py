1cb7f24f7002d0efdad746ed548c575dac630838
quamrana <wallguide-software@yahoo.com>
2022-12-31 17:08:41 +0000

    Improved test by creating new MockGenerator for each test.
    Now that I create a new mock class for each test, the tests become more independent.
    Test that any two decorated generators are different, but the wrapped generator is the same.

--- tests/TestCache.py
+++ tests/TestCache.py
@@ -3,28 +3,50 @@ from infinite import infinite_generator_cache
 
 
 # @infinite_generator_cache
-class MockGenerator:
-    number_of_calls = 0
-
-    def __init__(self):
-        MockGenerator.number_of_calls += 1
 
 
 class TestSingleGeneratorCached(TestCase):
     def setUp(self):
+        self.MockGenerator = self._create_new_mock_generator_class()
         self.thenNumberOfCallsIs(0)
+
+    @staticmethod
+    def _create_new_mock_generator_class():
+        class MockGenerator:
+            number_of_calls = 0
+
+            def __init__(self):
+                MockGenerator.number_of_calls += 1
+
+        return MockGenerator
 
     def test_Mock_Generator_already_called(self):
         self.whenFirstDecorationMade()
-        self.shouldEqual(MockGenerator.number_of_calls, 1)
+        self.thenNumberOfCallsIs(1)
         self.whenSecondDecorationMade()
-        self.shouldEqual(MockGenerator.number_of_calls, 1)
+        self.thenNumberOfCallsIs(1)
 
     def thenNumberOfCallsIs(self, expected):
-        self.shouldEqual(MockGenerator.number_of_calls, expected)
+        self.shouldEqual(self.MockGenerator.number_of_calls, expected)
 
     def whenFirstDecorationMade(self):
-        self.first = infinite_generator_cache(MockGenerator)
+        self.first = infinite_generator_cache(self.MockGenerator)
 
     def whenSecondDecorationMade(self):
-        self.second = infinite_generator_cache(MockGenerator)
+        self.second = infinite_generator_cache(self.MockGenerator)
+
+    def test_Decorators_themselves_are_different_instances(self):
+        self.whenFirstDecorationMade()
+        self.whenSecondDecorationMade()
+        self.thenTwoDecoratorsAreNotEqual()
+
+    def test_Wrapped_Generators_are_same_instance(self):
+        self.whenFirstDecorationMade()
+        self.whenSecondDecorationMade()
+        self.thenWrappedGeneratorsAreEqual()
+
+    def thenTwoDecoratorsAreNotEqual(self):
+        self.shouldNotEqual(self.first, self.second)
+
+    def thenWrappedGeneratorsAreEqual(self):
+        self.shouldEqual(self.first.wrapped, self.second.wrapped)
