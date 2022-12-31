e76cdebe75eb8f6d247b2b5bd378f83dfb5fd934
quamrana <wallguide-software@yahoo.com>
2022-12-31 16:26:22 +0000

    Fixed the cache using @classmethod
    Switched the _cache_of_wrapped_generators() method to a @classmethod. This ensures that @cache always sees the same first parameter and so subsequent calls are a cache-hit.

--- infinite/infinite.py
+++ infinite/infinite.py
@@ -1,13 +1,15 @@
 from functools import cache
 from itertools import count
 
+
 class infinite_generator_cache:
     def __init__(self, gen):
-        self.wrapped = self.cache_of_wrapped_generators(gen)
+        self.wrapped = self._cache_of_wrapped_generators(gen)
 
+    @classmethod
     @cache
-    def cache_of_wrapped_generators(self, gen):
-        return self._make_wrapped_generator(gen())  # gen is called exactly once just here
+    def _cache_of_wrapped_generators(cls, gen):
+        return cls._make_wrapped_generator(gen())  # gen is called exactly once just here
 
     @staticmethod
     def _make_wrapped_generator(gen):
