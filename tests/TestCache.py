789a9781bd972dc2a0cf4b061d5dbc19f8a87a47
quamrana <wallguide-software@yahoo.com>
2023-01-01 10:04:23 +0000

    First test for decorator caching results.
    Test now in place which fails if the wrapped generator  does not cache its results.

--- tests/TestCache.py
+++ tests/TestCache.py
@@ -1,24 +1,39 @@
 from qutest import TestCase
+from qutest import skip
 from infinite import infinite_generator_cache
 
 
 # @infinite_generator_cache
 
-
-class TestSingleGeneratorCached(TestCase):
-    def setUp(self):
-        self.MockGenerator = self._create_new_mock_generator_class()
-        self.thenNumberOfCallsIs(0)
-
+class CommonMethods:
     @staticmethod
     def _create_new_mock_generator_class():
         class MockGenerator:
             number_of_calls = 0
+            number_of_next = 0
 
             def __init__(self):
                 MockGenerator.number_of_calls += 1
 
+            def __iter__(self):
+                return self
+
+            def __next__(self):
+                MockGenerator.number_of_next += 1
+
         return MockGenerator
+    def whenFirstDecorationMade(self):
+        self.first = infinite_generator_cache(self.MockGenerator)
+    def whenFirstGeneratorIsCreated(self):
+        self.first_generator = self.first()
+        self.first_generators = []
+
+
+class TestSingleGeneratorCached(CommonMethods, TestCase):
+    def setUp(self):
+        self.MockGenerator = self._create_new_mock_generator_class()
+        self.thenNumberOfCallsIs(0)
+
 
     def test_Mock_Generator_already_called(self):
         self.whenFirstDecorationMade()
@@ -29,8 +44,6 @@ class TestSingleGeneratorCached(TestCase):
     def thenNumberOfCallsIs(self, expected):
         self.shouldEqual(self.MockGenerator.number_of_calls, expected)
 
-    def whenFirstDecorationMade(self):
-        self.first = infinite_generator_cache(self.MockGenerator)
 
     def whenSecondDecorationMade(self):
         self.second = infinite_generator_cache(self.MockGenerator)
@@ -50,16 +63,13 @@ class TestSingleGeneratorCached(TestCase):
         self.thenFirstAsStringIncludesAll(['infinite_generator_cache object', 'containing <class',
                                            '_create_new_mock_generator_class.<locals>.MockGenerator'])
         self.whenFirstGeneratorIsCreated()
-        self.thenFirstGeneratorAsStringIs('<generator object TestSingleGeneratorCached._create_new_mock_generator_class.<locals>.MockGenerator')
+        self.thenFirstGeneratorAsStringIs('<generator object CommonMethods._create_new_mock_generator_class.<locals>.MockGenerator')
 
     def thenTwoDecoratorsAreNotEqual(self):
         self.shouldNotEqual(self.first, self.second)
 
     def thenWrappedGeneratorsAreEqual(self):
         self.shouldEqual(self.first.wrapped, self.second.wrapped)
-
-    def whenFirstGeneratorIsCreated(self):
-        self.first_generator = self.first()
 
     def thenFirstAsStringIncludesAll(self, expected):
         str_repr = str(self.first)
@@ -70,3 +80,31 @@ class TestSingleGeneratorCached(TestCase):
         str_repr = str(self.first_generator)
         result = str_repr.startswith(expected)
         self.shouldBeTrue(result, f'{expected} should be in {str_repr}')
+
+class TestValuesNotCachedUntilSecondInstanceStarts(CommonMethods, TestCase):
+    def setUp(self):
+        self.MockGenerator = self._create_new_mock_generator_class()
+
+    def test_first_few_calls_to_next_not_cached(self):
+        self.whenFirstDecorationMade()
+        self.whenFirstGeneratorIsCreated()
+        self.whenFirstGeneratorGivesSamples(10)
+        self.thenGeneratorShowsCall(10)
+        self.whenFirstGeneratorIsCreatedAgain()
+        self.whenFirstGeneratorsAreSampled(10)
+        self.thenGeneratorShowsCall(10)
+
+    def whenFirstGeneratorGivesSamples(self, limit):
+        for _ in range(limit):
+            next(self.first_generator)
+
+    def thenGeneratorShowsCall(self, expected):
+        self.shouldEqual(self.MockGenerator.number_of_next, expected)
+
+    def whenFirstGeneratorIsCreatedAgain(self):
+        self.first_generators.append(self.first())
+
+    def whenFirstGeneratorsAreSampled(self, limit):
+        for _ in range(limit):
+            for g in self.first_generators:
+                next(g)
